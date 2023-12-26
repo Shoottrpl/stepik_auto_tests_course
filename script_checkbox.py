30be458f81abc0b7387c1d047c1a9d2c6a49b298
Shoottrpl <triplsix1337@gmail.com>
2023-12-26 23:48:56 +0300

    selenium script for checkbox

---
+++ script_checkbox.py
@@ -0,0 +1,32 @@
+import math
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+try:
+    link = "http://suninjuly.github.io/get_attribute.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+    x = browser.find_element(By.CSS_SELECTOR,'#treasure').get_attribute('valuex')
+    browser.find_element(By.CSS_SELECTOR, '#answer').send_keys(calc(x))
+    for val in '#robotCheckbox', '#robotsRule':
+        browser.find_element(By.CSS_SELECTOR,f'[for="{val}"]').click()
+
+
+    # Отправляем заполненную форму
+    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+    button.click()
+
+    # Проверяем, что смогли зарегистрироваться
+    # ждем загрузки страницы
+    time.sleep(1)
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
